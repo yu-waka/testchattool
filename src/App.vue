852e87335080faac3f56cf7cfaf61447b4a52a87
yu-waka <>
2020-04-08 15:31:02 +0900

    layout adjust

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <div v-if="this.$store.state.user">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/chat">chat</router-link>|
-        <amplify-sign-out></amplify-sign-out>
-      </div>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header>
+        <navi/>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
+
+<script>
+import navi from './components/Navi'
+export default {
+  name:'App',
+  components:{
+    navi
+  }
+}
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/Navi.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav" v-if="this.$store.state.user">
+    <div id="nav-item">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/chat">chat</router-link>
+    </div>
+    <div id="nav-item-signout">
+      <amplify-sign-out></amplify-sign-out>
+    </div>
+      <!-- <el-row >
+        <el-col :span=16 >
+        </el-col>
+        <el-col :span=8>
+          <amplify-sign-out></amplify-sign-out>
+        </el-col>
+      </el-row> -->
+  </div>
+</template>
+<style scoped>
+#nav{
+  display: table;
+  width: 100%;
+}
+#nav-item{
+  display: table-cell;
+  vertical-align: middle;
+  text-align: left;
+}
+#nav-item-signout{
+  display: table-cell;
+  text-align: right;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+/* .el-row {
+  margin: auto;
+}
+.el-col {
+  text-align: center;
+} */
+</style>>
