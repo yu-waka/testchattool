37536890741adf3b54ffbf91420e54b7e5e5d8ca
yu-waka <>
2020-04-09 17:35:31 +0900

    チャットルーム一覧の取得、更新（追加時）処理を追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,19 @@
             <el-input placeholder="create room" v-model="roomName" @keydown.enter.native="createRoom"></el-input>
       </el-col>
     </el-row>
+    <div class="roomlist_base">
+      <div v-for="room in rooms" :key="room.id">
+        {{room.name}}
+      </div>
+    </div>
     <div class="error"> {{this.error}}</div>
   </div>
 </template>
 <script>
 import API, { graphqlOperation } from '@aws-amplify/api'
 import { createChatroom } from '../graphql/mutations'
+import { listChatrooms } from '../graphql/queries'
+import { onCreateChatroom } from '../graphql/subscriptions'
 
 export default {
   name:'Home',
@@ -21,6 +28,8 @@ export default {
     return{
       username:'',
       roomName: '',
+      rooms:[],
+      subscription: {},
       error: ''
     }
   },
@@ -30,9 +39,7 @@ export default {
     }
   },
   methods:{
-    async created(){
-      this.username = this.$store.getters.getUserName
-    },
+    //ルーム作成
     createRoom(){
       if (event.keyCode !== 13) return
       const param = {
@@ -43,7 +50,29 @@ export default {
       API.graphql(graphqlOperation(createChatroom,{input:param}))
       .catch(error => this.error = JSON.stringify(error))
       this.roomName = ""
+    },
+    //レコード一覧の取得
+    fetch(){
+      API.graphql(graphqlOperation(listChatrooms,{limit: 100}))
+      .then(rooms => this.rooms =rooms.data.listChatrooms.items)
+      .catch(error => this.error = JSON.stringify(error))
+    },
+    subscribe(){
+    this.subscription = API.graphql(graphqlOperation(onCreateChatroom))
+      .subscribe({
+        next: (eventData) => {
+          const room = eventData.value.data.onCreateChatroom
+          this.rooms.push(room)
+        }
+      })
     }
+  },
+  async created(){
+    this.fetch()
+    this.subscribe()
+  },
+  beforeDestoroy(){
+    this.subscription.unsubscribe()
   }
 }
 </script>>
